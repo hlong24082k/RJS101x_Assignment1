91113eb5b280070bb838e73c015bad9ff1443be6
unknown <hlong.24082k@gmail.com>
2022-03-31 15:20:10 +0700

    Long

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { Navbar,NavbarBrand} from 'reactstrap';
+import StaffList from './components/StaffListComponent';
 import './App.css';
+import {STAFFS} from './shared/staffs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      staffs : STAFFS,
+    };
+  }
+  
+  render(){
+    console.log('Menu.props');
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+            </div>
+          </Navbar>
+          
+          <StaffList nhanvien = {this.state.staffs}/>
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+import {Card,CardText, CardBody, CardTitle} from 'reactstrap';
+import dateFormat from "dateformat";
+import "../css/style.css";
+class StaffList extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            selected_nhanvien:null
+        }  
+    }
+    onnvSelect(staffs) {
+        this.setState({ selected_nhanvien:staffs});
+        
+    };
+    rendernhanvien(a){
+        if(a == null){
+            return(
+                <div>
+                    <p>Bấm vào tên nhân viên để xem thông tin</p>
+                </div>
+            )
+        }
+        else{
+            return(
+                <div className='row display'>
+                <div className="col-12 col-sm-6 col-md-6">
+                <Card>
+                    <CardBody>
+                        <h6> Họ và tên: {a.name}</h6>
+                        <p>Ngày sinh: {dateFormat(a.doB, "dd/mm/yyyy")}</p>
+                        <p>Ngày vào công ty: {dateFormat(a.startDate, "dd/mm/yyyy")}</p>
+                        <p>Phòng ban: {a.department.name}</p>
+                        <p>Số ngày nghỉ còn lại:{a.annualLeave}</p>
+                        <p>Số ngày đã làm thêm:{a.overTime}</p>
+                        <CardText></CardText>
+                    </CardBody>
+                </Card>
+                </div>
+                </div>
+            )
+        }
+    };
+    render(){
+        const menu = this.props.nhanvien.map((staffs)=>{
+            console.log(this.props.socot);
+            var x;
+            x = "col-12 col-sm-6 col-md-4 infor";
+                 
+            if (this.state.selected_nhanvien!=null){
+                if(this.state.selected_nhanvien.id ==staffs.id){
+                    return(
+                        <div key={staffs.id} className= {x}>
+                            <Card onClick={()=>this.onnvSelect(staffs)}>
+                                 <CardTitle style ={{color:'red'}}>{staffs.name}</CardTitle>   
+                            </Card>
+                        </div>
+                    )}
+                else{
+                    return(
+                        <div key={staffs.id} className= {x}>
+                            <Card onClick={()=>this.onnvSelect(staffs)}>
+                                <CardTitle>{staffs.name}</CardTitle>   
+                            </Card>
+                        </div>
+                    )}
+
+                }
+            else{
+                return( <
+                    div key={staffs.id} className= {x}>
+                        <Card onClick={()=>this.onnvSelect(staffs)}>
+                            <CardTitle>{staffs.name}</CardTitle>   
+                        </Card>
+                    </div>
+                );}
+            });
+        return (
+            <div className="container">
+                <div className="row">
+                        {menu}
+                </div>
+                    {this.rendernhanvien(this.state.selected_nhanvien)}
+            </div>
+        );
+    }
+}
+export default StaffList;
+
+  
